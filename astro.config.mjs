cec2b0c8b202eac409c6ce9f1fdd1b4111dcfb0a
Tu Nombre <tu_email@ejemplo.com>
2026-03-24 11:12:53 -0600

    Primer commit Lazaro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,6 @@
-// @ts-check
 import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind'; // Asegúrate de que esta línea esté presente
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()], // Y esta también
+});
